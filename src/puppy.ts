0b6ed5a41a6ca9c582b81290dce53fcf592b16b6
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-28 23:46:43 +0200

    clean up puppy.ts

--- src/puppy.ts
+++ src/puppy.ts
@@ -4,17 +4,12 @@ import { promises as fs } from 'fs';
 async function executeJavaScriptFile(htmlFilePath: string, jsCode: string) : Promise<any>  {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  // Load the HTML file
-  //console.log("Inside Puppeteer");
   const htmlContent = await fs.readFile(htmlFilePath, 'utf8');
-  //console.log("Behind readFile");
   await page.setContent(htmlContent);
-  // Execute the JavaScript code
   const result = await page.evaluate(jsCode);
   await browser.close();
-  return result; //console.log(result);
+  return result; 
 }
 
-//executeJavaScriptFile("index.html", "document.querySelectorAll('header a')[0].textContent");
 
 export default executeJavaScriptFile
