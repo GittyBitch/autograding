e48ff1f246ff90408f6aba999a2a93bf19f3f1e7
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-31 01:31:47 +0100

    fix up puppy.ts which wasn't resolving referenced resources so far

--- src/puppy.ts
+++ src/puppy.ts
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer';
-import { promises as fs } from 'fs';
+//import { promises as fs } from 'fs';
+import * as path from 'path';
 
 async function executeJavaScriptFile(htmlFilePath: string, jsCode: string) : Promise<any>  {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  const htmlContent = await fs.readFile(htmlFilePath, 'utf8');
-  await page.setContent(htmlContent);
+  //const htmlContent = await fs.readFile(htmlFilePath, 'utf8');
+  const fullPath = path.join(process.cwd(), htmlFilePath);
+  await page.goto('file://'+ fullPath, { waitUntil: 'load' });
   const result = await page.evaluate(jsCode);
   await browser.close();
   return result; 
