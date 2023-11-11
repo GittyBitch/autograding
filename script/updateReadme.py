0d67151a95f5126d6986f1d6d3e82b4647695df0
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-11 12:37:01 +0100

    Update updateReadme.py

--- script/updateReadme.py
+++ script/updateReadme.py
@@ -8,27 +8,6 @@ def write_intro(file, title, content):
     if content is not None:
         file.write(f"# {title}\n")
         file.write(f"{content}\n")
-
-
-def write_section(file, title, content):
-    if content is not None:
-        file.write(f"#### {title}\n")
-        file.write(f"{content}\n")
-
-
-def write_smallsection(file, title, content):
-    file.write(f"##### {title}\n")
-    file.write(f"* {content}\n")
-
-
-def write_smallersection(file, title, content):
-    file.write(f"###### {title}\n")
-    file.write(f"* {content}\n")
-
-
-def write_cheatsheet_section(file, title, content):
-    file.write(f"###### {title}\n")
-    file.write(f"{content}\n")
     
 def write_horizontal_line(file):
     file.write(f"\n---\n")
@@ -39,13 +18,14 @@ if __name__ == "__main__":
         data = json.load(autograding_file)
     
     with open(readme, 'w', encoding="utf-8") as readme_file:
-        repo = data.get("repo-name")
+        repo = data.get("repo")
         introduction = ""
-        if repo != None:
-            statusURL = f"https://github.com/{repo}/actions/workflows/classroom.yml/badge.svg"
-            introduction = f"[![GitHub Classroom Workflow]({statusURL}/badge.svg)]({statusURL})"
+        if repo != None or repo != False:
+            statusURL = f"https://github.com/{repo}/actions/workflows/classroom.yml"
+            introduction = f"[![GitHub Classroom Workflow]({statusURL}/badge.svg)]({statusURL}) \n\n" # FIXME: Der Link stimmt nicht mehr so ganz ...
 
-        introduction += data.get("introduction")
+        textIntro = data.get("introduction")
+        introduction += textIntro
         total_points = 0
         for p in data.get("tests"):
             total_points += int(p.get("points",0))
@@ -53,39 +33,41 @@ if __name__ == "__main__":
         timeframe = data.get("timeframe", "30 Minuten")
         
         if logo_url is not None:
-            readme_file.write(f"<img src=\"{logo_url}\" alt=\"{introduction}\" width=\"300\"/>\n")
+            readme_file.write(f"<img src=\"{logo_url}\" alt=\"{textIntro}\" width=\"300\"/>\n")
             write_horizontal_line(readme_file)
         
         if introduction is not None:
             write_intro(readme_file, "Aufgabe", introduction)
             readme_file.write(f"* {total_points} Punkte\n* {timeframe}\n")
             write_horizontal_line(readme_file)
-        
+            hint = "Ladet eure Lösung bitte nicht in den .github-Ordner. "
+            readme_file.write(f"<p>{hint} </p>\n")
+            write_horizontal_line(readme_file)
+        readme_file.write(f"<ol>\n")
         for test in data.get("tests"):
             have_specs = test.get("specs")
             points = test.get("points")
             title = have_specs.get("title")
-            readme_file.write(f"### {title} ({points} Punkte)\n")
+            readme_file.write(f"<li> {title} ({points} Punkte)</li>\n")
             
-            if have_specs is not None:
-                name = test.get("name")
-                write_section(readme_file, name, "")
                 
-                if "list" in have_specs:
-                    content = ""
-                    for l in have_specs["list"]:
-                        content += (l + "\n")
-                        write_smallsection(readme_file, "Unteraufgaben", l)
-                        
-                if "code_example" in have_specs:
-                    write_smallersection(readme_file, "Code-Beispiel:", f"`{have_specs.get('code_example')}`")
+            if "list" in have_specs:
+                for l in have_specs["list"]:
+                    readme_file.write(f"<ul><li> {l}</li></ul>\n") 
+                    
+            if "code-example" in have_specs:
+                readme_file.write(f"Code-Beispiel: \n")
+                readme_file.write(f"<ul><li><code>{have_specs.get('code-example')}</code></li></ul>\n")
+            
+            if "urls" in test:
+                readme_file.write(f"Hilfe: \n")
+                for spickzettel in test["urls"]:
+                    #readme_file.write(f"<ul><li>[Spickzettel]({spickzettel})</li></ul> \n")
+                    readme_file.write(f"<ul><li><a href=\"{spickzettel}\">Spickzettel</a></li></ul> \n")
+
+            write_horizontal_line(readme_file)
+        readme_file.write(f"</ol>\n")
                 
-                if "urls" in test:
-                    content = ""
-                    for l in test["urls"]:
-                        content += (f"* [Spickzettel]({l}) \n")
-                    write_cheatsheet_section(readme_file, "Hilfe", content)
-                    write_horizontal_line(readme_file)
                 
     readme_file.close()
     autograding_file.close()
